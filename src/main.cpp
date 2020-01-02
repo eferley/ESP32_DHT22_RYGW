c19749a21fda3e03374786ed8e19c9a49a884fee
Eric Ferley <eferley@users.noreply.github.com>
2020-01-02 11:24:51 +0100

    Add DHT22 management with 4-color LEDs output

--- src/main.cpp
+++ src/main.cpp
@@ -7,39 +7,99 @@
 #define LED_GREEN_PIN     16
 #define LED_WHITE_PIN     5
 
+#define LED_ON            HIGH
+#define LED_OFF           LOW
+
+#define DHTPIN            23
+#define DHTTYPE           DHT22
+#define DHTMEASURETIME    2000
+
+#define TEMP_RED          22.0        // Red LED above this temp.
+#define TEMP_YELLOW       21.0        // Yellow LED above this temp.
+#define TEMP_GREEN        19.5        // Green LED above / White LED under this temp.
+
+DHT dhtSensor(DHTPIN, DHTTYPE);       // Temp./Humidity sensor module
+
+float fTmp;               // Temperature (Celcius)
+float fHum;               // Humidity (percent)
+float fHtIdx;             // Heat Index (Celcius)
+float fSndSpd;            // Sound Speed (m/s)
+
+unsigned long ulTime;     // current time (milliseconds)
+unsigned long ulT0;       // last measurement time (milliseconds)
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
 
+  // pinMode(DHTPIN, INPUT_PULLUP);
+
   pinMode(LED_RED_PIN, OUTPUT);
   pinMode(LED_YELLOW_PIN, OUTPUT);
   pinMode(LED_GREEN_PIN, OUTPUT);
   pinMode(LED_WHITE_PIN, OUTPUT);
+
+  dhtSensor.begin();
+  
+  digitalWrite(LED_RED_PIN, LED_OFF);
+  digitalWrite(LED_YELLOW_PIN, LED_OFF);
+  digitalWrite(LED_GREEN_PIN, LED_OFF);
+  digitalWrite(LED_WHITE_PIN, LED_OFF);
+
+  ulT0 = millis();
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
+  ulTime = millis();
+  if (ulTime - ulT0 > DHTMEASURETIME)       // Take a measurement every DHTMEASURETIME milliseconds
+  {
+    fTmp = dhtSensor.readTemperature(false);
+    fHum = dhtSensor.readHumidity();
+    // Get Heat Index
+    fHtIdx = dhtSensor.computeHeatIndex(fTmp, fHum, false);
+    // Calculate the Speed of Sound in m/s
+    fSndSpd = 331.4 + (0.606 * fTmp) + (0.0124 * fHum);
 
-  digitalWrite(LED_RED_PIN, HIGH);
-  digitalWrite(LED_YELLOW_PIN, HIGH);
-  digitalWrite(LED_GREEN_PIN, HIGH);
-  digitalWrite(LED_WHITE_PIN, HIGH);
+    ulT0 = ulTime;
 
-  Serial.print("LED_BUILTIN="); Serial.print(LED_BUILTIN);
-  Serial.print(" Status="); Serial.println(digitalRead(LED_BUILTIN) == HIGH ? "HIGH" : "LOW");
-  // wait for a second
-  delay(2000);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+    Serial.print("Temp.  : "); Serial.print(fTmp, 1); Serial.print(" C");
+    Serial.print(" - Humid. : " ); Serial.print(fHum, 1); Serial.print(" %");
+    Serial.print(" - Heat Idx. : " ); Serial.print(fHtIdx, 1); Serial.print(" C");
+    Serial.print( " - Snd.Sp.: " ); Serial.print(fSndSpd, 1); Serial.print( " m/s " );
+    Serial.println();
 
-  digitalWrite(LED_RED_PIN, LOW);
-  digitalWrite(LED_YELLOW_PIN, LOW);
-  digitalWrite(LED_GREEN_PIN, LOW);
-  digitalWrite(LED_WHITE_PIN, LOW);
-
-  Serial.print("LED_BUILTIN="); Serial.print(LED_BUILTIN);
-  Serial.print(" Status="); Serial.println(digitalRead(LED_BUILTIN) == HIGH ? "HIGH" : "LOW");
-   // wait for a second
-  delay(1000);
+    if (fTmp >= TEMP_RED) {
+      Serial.println(" => Red");
+      digitalWrite(LED_RED_PIN, LED_ON);
+      digitalWrite(LED_YELLOW_PIN, LED_OFF);
+      digitalWrite(LED_GREEN_PIN, LED_OFF);
+      digitalWrite(LED_WHITE_PIN, LED_OFF);
+    }
+    else if (fTmp >= TEMP_YELLOW) {
+      Serial.println(" => Yellow");
+      digitalWrite(LED_RED_PIN, LED_OFF);
+      digitalWrite(LED_YELLOW_PIN, LED_ON);
+      digitalWrite(LED_GREEN_PIN, LED_OFF);
+      digitalWrite(LED_WHITE_PIN, LED_OFF);
+    }
+    else if (fTmp >= TEMP_GREEN) {
+      Serial.println(" => Green");
+      digitalWrite(LED_RED_PIN, LED_OFF);
+      digitalWrite(LED_YELLOW_PIN, LED_OFF);
+      digitalWrite(LED_GREEN_PIN, LED_ON);
+      digitalWrite(LED_WHITE_PIN, LED_OFF);
+    }
+    else {
+      Serial.println(" => White");
+      digitalWrite(LED_RED_PIN, LED_OFF);
+      digitalWrite(LED_YELLOW_PIN, LED_OFF);
+      digitalWrite(LED_GREEN_PIN, LED_OFF);
+      digitalWrite(LED_WHITE_PIN, LED_ON);
+    }
+  } // if (ulTime - ulT0 > DHTMEASURETIME)
+  else
+  {
+    delayMicroseconds(100);
+  }
+  
 }
