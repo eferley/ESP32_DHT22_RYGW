c1d48b49ef3ef92f8936cdeac13dc4f4fa11f915
Eric Ferley <eferley@users.noreply.github.com>
2020-01-02 09:32:33 +0100

    RYGW LEDs

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,44 @@
 
 #include "DHT.h"
 
+#define LED_RED_PIN       0
+#define LED_YELLOW_PIN    2
+#define LED_GREEN_PIN     16
+#define LED_WHITE_PIN     5
+
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_YELLOW_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_WHITE_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  digitalWrite(LED_RED_PIN, HIGH);
+  digitalWrite(LED_YELLOW_PIN, HIGH);
+  digitalWrite(LED_GREEN_PIN, HIGH);
+  digitalWrite(LED_WHITE_PIN, HIGH);
+
+  Serial.print("LED_BUILTIN="); Serial.print(LED_BUILTIN);
+  Serial.print(" Status="); Serial.println(digitalRead(LED_BUILTIN) == HIGH ? "HIGH" : "LOW");
+  // wait for a second
+  delay(2000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+
+  digitalWrite(LED_RED_PIN, LOW);
+  digitalWrite(LED_YELLOW_PIN, LOW);
+  digitalWrite(LED_GREEN_PIN, LOW);
+  digitalWrite(LED_WHITE_PIN, LOW);
+
+  Serial.print("LED_BUILTIN="); Serial.print(LED_BUILTIN);
+  Serial.print(" Status="); Serial.println(digitalRead(LED_BUILTIN) == HIGH ? "HIGH" : "LOW");
+   // wait for a second
+  delay(1000);
 }
